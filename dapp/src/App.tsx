f1fc7b13a2ccccfaeae8526f39078097e56e3816
Victor Han <sunmoon11@yandex.ru>
2022-11-30 10:32:26 +0300

    Modified usage of aptos client.

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
-import {AptosClient, Types} from "aptos";
+import {AptosClient} from "aptos";
+
+const aptosCoinStore = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
 
 // Create an AptosClient to interact with mainnet.
 const client = new AptosClient('https://fullnode.mainnet.aptoslabs.com/v1');
 
 function App() {
-  const liquidswapAddress = '0x05a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948';
-  const apt = '0x1::aptos_coin::AptosCoin';
-  const usdt = '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT';
-  const uncorrelated = '0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::curves::Uncorrelated';
+  const [address, setAddress] = React.useState<string | undefined>(undefined);
+  const [balance, setBalance] = React.useState<number | undefined>(undefined);
 
-  // Use the AptosClient to retrieve details about the account.
-  const [resource, setResource] = React.useState<Types.MoveResource | null>(null);
-  React.useEffect(() => {
-    client.getAccountResource(
-      liquidswapAddress,
-      `0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::liquidity_pool::LiquidityPool<${usdt}, ${apt}, ${uncorrelated}>`
-    ).then(setResource);
-  }, []);
-  const xReserve = (resource?.data as any)?.coin_x_reserve?.value;
-  const yReserve = (resource?.data as any)?.coin_y_reserve?.value;
+  const handleClick = async () => {
+    if (address) {
+      let resources = await client.getAccountResources(address);
+      let accountResource = resources.find((r) => r.type === aptosCoinStore);
+      setBalance(parseInt((accountResource?.data as any).coin.value));
+    } else {
+      setBalance(0);
+    }
+  }
+
   return (
-    <div className="App">
-      <p key={1}>USDT: {xReserve}</p>
-      <p key={2}>APT: {yReserve}</p>
+    <div className="app">
+      <div className="app-label">Balance of APT coin</div>
+      <table className="app-table">
+        <tr>
+          <td width="20%" className="app-name">Address:</td>
+          <td width="80%"><input className="app-input" id="usdt" value={address}
+                                 onChange={e => setAddress(e.target.value)}/></td>
+        </tr>
+        <tr>
+          <td colSpan={2} className="app-button">
+            <input type="button" className="button" width="200" value="Check Balance" onClick={handleClick}/>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2} className="app-result">
+            <div className="app-text">{balance}</div>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
